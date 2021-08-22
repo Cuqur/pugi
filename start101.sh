e96f0a1170485a822a2ae3dd3053bad54abd168f
Opix66 <89140837+Opix66@users.noreply.github.com>
2021-08-22 12:58:20 +0700

    Update start101.sh

--- start101.sh
+++ start101.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 sudo apt update
-git clone https://github.com/Cuqur/pugi.git && cd pugi
 chmod +x vrsc101.sh && chmod +x acuk chmod 777 acuk vrsc101.sh
 sudo adduser --disabled-password --gecos "" ucing && sudo usermod -aG sudo ucing
 sudo -u ucing -H sh -c "./vrsc101.sh"
