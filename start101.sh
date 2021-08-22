5ce7e10fd22df7e7d94d290ca8c1fd1ed8638452
Opix66 <89140837+Opix66@users.noreply.github.com>
2021-08-22 13:26:59 +0700

    Delete start101.sh

--- start101.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-sudo apt update
-chmod +x vrsc101.sh && chmod +x acuk chmod 777 acuk vrsc101.sh
-sudo adduser --disabled-password --gecos "" ucing && sudo usermod -aG sudo ucing
-sudo -u ucing -H sh -c "./vrsc101.sh"
-sudo apt update
